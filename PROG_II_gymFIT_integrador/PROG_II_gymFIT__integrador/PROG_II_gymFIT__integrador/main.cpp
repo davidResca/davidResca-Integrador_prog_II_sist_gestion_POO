ec6196a4bbd546c17b025670944feb3f03a604db
david r <davidresca9@gmail.com>
2024-10-24 12:34:37 -0300

    Agrega: clases Persona, Fecha, Empleado

---
+++ PROG_II_gymFIT_integrador/PROG_II_gymFIT__integrador/PROG_II_gymFIT__integrador/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "Fecha.h"
+#include "Empleado.h"
+
+
+int main() {
+	
+
+
+	/*try {
+		fecha = Fecha(29, 2, 2023);
+		std::cout << fecha << std::endl;
+	}
+	catch (const std::invalid_argument& e) {
+		std::cerr << "ooops... fecha incorrecta, asignare una fecha por default"<<std::endl;
+	}*/
+
+
+	return 0;
+}                            
